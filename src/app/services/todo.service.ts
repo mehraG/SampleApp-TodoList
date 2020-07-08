5e489c0e076d27b97e25d8f0de0297999a1a2035
mehraG <46636885+mehraG@users.noreply.github.com>
2020-07-08 23:51:50 +0530

    Make TodoList App

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo';
+const httpOptions = {
+  headers:new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todosUrl:string= 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
+
+  constructor(private http:HttpClient) { }
+
+  getTodos():Observable<Todo[]> {
+    // return [
+    //   {
+    //     id:1,
+    //     title: 'Todo One',
+    //     completed: false
+    //   },
+    //   {
+    //     id:2,
+    //     title: 'Todo Two',
+    //     completed: true
+    //   },
+    //   {
+    //     id:3,
+    //     title: 'Todo Three',
+    //     completed: false
+    //   },
+    // ]
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  deleteTodo(todo:Todo):Observable<Todo>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions)
+  }
+}
